d383980261057d6396402bedb1eca611ff8efa84
Yudong <qiuyudong4@gmail.com>
2025-04-17 10:11:36 +1000

    test

--- app/src/main/java/com/example/parkingreport/ui/admin/fragment/unreFragment.java
+++ app/src/main/java/com/example/parkingreport/ui/admin/fragment/unreFragment.java
@@ -18,37 +18,35 @@ import com.example.parkingreport.ui.admin.fragment.review.ItemAdapter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ReviewListFragment extends Fragment {
+public class unreFragment extends Fragment {
+
     private RecyclerView recyclerView;
 
 
     private ItemAdapter adapter;
 
-    public ReviewListFragment() {
+    public unreFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_review_list, container, false);
-
+        return inflater.inflate(R.layout.fragment_unre, container, false);
     }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         // 初始化 RecyclerView
-        recyclerView = view.findViewById(R.id.recycle);
+        recyclerView = view.findViewById(R.id.recycle2);
 
         List<Item> itemList = new ArrayList<>();
-        itemList.add(new Item("Test 1"));
-        itemList.add(new Item("Test 2"));
-        itemList.add(new Item("Test 3"));
+        itemList.add(new Item("Test 4"));
+        itemList.add(new Item("Test 5"));
+        itemList.add(new Item("Test 6"));
 
         adapter = new ItemAdapter(itemList, getContext());
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(adapter);
     }
 }
-
