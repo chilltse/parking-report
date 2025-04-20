fbd6a5be2e58b65730068b91940418bd5bad8c78
nanxuan.xie <u8016457@anu.edu.au>
2025-04-20 14:38:54 +1000

    fix based on MR review

--- app/src/main/java/com/example/parkingreport/ui/admin/fragment/unreview/UnreviewAdapter.java
+++ app/src/main/java/com/example/parkingreport/ui/admin/fragment/unreview/UnreviewAdapter.java
@@ -42,7 +42,7 @@ public class UnreviewAdapter extends RecyclerView.Adapter<UnreviewAdapter.ItemVi
         ItemForUnre currentItem = itemList.get(position);
         holder.title.setText(currentItem.getItem());
 
-        // 点击事件：跳转到 DetailActivity
+        // Click event, jump to DetailActivity
         holder.itemView.setOnClickListener(view -> {
             Intent intent = new Intent(context, Unreview_list_deatilActivity.class);
             intent.putExtra("title", currentItem.getItem());
