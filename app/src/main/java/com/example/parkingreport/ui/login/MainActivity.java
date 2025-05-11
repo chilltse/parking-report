342e9de67fa59c38d16423eecffebcf43a68f4b6
Yudong <qiuyudong4@gmail.com>
2025-05-11 19:05:41 +1000

    bug fix

--- app/src/main/java/com/example/parkingreport/ui/login/MainActivity.java
+++ app/src/main/java/com/example/parkingreport/ui/login/MainActivity.java
@@ -7,7 +7,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
