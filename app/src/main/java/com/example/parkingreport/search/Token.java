2d79c036ade6f93e2dc8128bfe21332cf5767ca5
larrywang <u7807744@anu.edu.au>
2025-05-11 16:18:29 +1000

    refactor token,tokenizer, parser; delete SearchEngine.java.

--- app/src/main/java/com/example/parkingreport/search/Token.java
+++ app/src/main/java/com/example/parkingreport/search/Token.java
@@ -1,5 +1,7 @@
 package com.example.parkingreport.search;
 
+import androidx.annotation.Nullable;
+
 /**
  * Represents a token with a specific type and value.
  * Used in search parsing to represent parsed units (e.g., plate numbers, names).
@@ -7,6 +9,8 @@ package com.example.parkingreport.search;
 public class Token {
     String type;
     String value;
+    public static final String USERNAME = "userName";
+    public static final String CARPLATE = "carPlate";
 
     // Initializes a token with the given type and value.
     public Token(String type, String value){
@@ -18,4 +22,28 @@ public class Token {
     public void setType(String type) { this.type = type; }
     public String getValue() { return value; }
     public void setValue(String value) { this.value = value; }
+
+    @Override
+    public boolean equals(@Nullable Object o) {
+        if (this == o) return true;  // 同一个对象
+        if (o == null || getClass() != o.getClass()) return false;  // 类型不符
+        Token token = (Token) o;
+        return this.type.equals(((Token) o).type) && this.value.equals(((Token) o).value);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + (type == null ? 0 : type.hashCode());
+        result = 31 * result + (value == null ? 0 : value.hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Token{" +
+                "TYPE=" + type +
+                ", VALUE=" + value +
+                '}';
+    }
 }
